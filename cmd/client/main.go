6d7dc024798867edb122bdce3d542a80ccd35794
Nikita Lebedev <lebedev@greensight.ru>
2022-05-09 00:13:05 +0300

    init

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,1 +1,73 @@
 package main
+
+import (
+	"fmt"
+	_ "github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/examples/resources/fonts"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/opentype"
+	"image"
+	"image/color"
+	_ "image/png"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/text"
+)
+
+const (
+	screenWidth  = 320
+	screenHeight = 240
+)
+
+var (
+	runnerImage *ebiten.Image
+)
+
+type Game struct {
+	Login image.Rectangle
+	count int
+}
+
+func (g *Game) Update() error {
+	g.count++
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	tt, err := opentype.Parse(fonts.MPlus1pRegular_ttf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var mplusNormalFont font.Face
+	mplusNormalFont, err = opentype.NewFace(tt, &opentype.FaceOptions{
+		Size:    24,
+		DPI:     78,
+		Hinting: font.HintingFull,
+	})
+
+	g.Login = text.BoundString(mplusNormalFont, "Login")
+	text.Draw(screen, "Login", mplusNormalFont, screenWidth/2, screenHeight/3, color.White)
+	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
+		x, y := ebiten.CursorPosition()
+
+		if g.Login.RGBA64At(x-screenWidth/2, y-screenHeight/3).A > 0 {
+			fmt.Print("click")
+		}
+
+	}
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
+}
+
+func main() {
+	ebiten.SetWindowSize(screenWidth*2, screenHeight*2)
+	ebiten.SetWindowTitle("Endor")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -8,8 +8,9 @@ require (
 	github.com/hajimehoshi/ebiten/v2 v2.3.1 // indirect
 	github.com/jezek/xgb v1.0.0 // indirect
 	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
-	golang.org/x/image v0.0.0-20220321031419-a8550c1d254a // indirect
+	golang.org/x/image v0.0.0-20220413100746-70e8d0d3baa9 // indirect
 	golang.org/x/mobile v0.0.0-20220325161704-447654d348e3 // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20220408201424-a24fb2fb8a0f // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
